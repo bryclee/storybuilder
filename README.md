4168ee3feed87c67fef38626d2b7561bef768eca
Bryan Lee <pygmize.me@gmail.com>
2015-05-21 20:23:27 -0700

    Adds anonymous authorization, README

---
+++ README.md
@@ -0,0 +1,10 @@
+#Storybuilder
+
+Make a one-word story with whoever's on the webpage! Users are limited to only typing one word every second. Try to collaborate with others to create an interesting story!
+
+##Demo
+https://storybuilder.firebaseapp.com
+
+##About
+
+This project was made in roughly 2 hours during a mini-hackathon. The prompt was to create something using Firebase. The key feature I wanted to use was the reactive data updates, so I came up with this idea.
--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,21 @@ var nodes;
 var lastWord;
 var inputReady = true;
 
+firebaseRef.authAnonymously(function(error, authData) {
+  if (error) {
+    console.log(error);
+    return;
+  } else {
+    console.log('authenticated', authData);
+  }
+}, {
+  remember: 'sessionOnly'
+});
+
 storyRef.on('child_added', function(childSnapshot) {
   lastWord = childSnapshot;
   var node = document.createElement('span');
-  node.innerText = ' ' + childSnapshot.val();
+  node.appendChild(document.createTextNode(' ' + childSnapshot.val()));
 
   node.className = 'last';
   nodes = storyDiv.childNodes;
@@ -24,8 +35,9 @@ storyRef.on('child_added', function(childSnapshot) {
 
 storyRef.on('child_removed', function(oldChildSnapshot) {
   storyDiv.removeChild(nodes[nodes.length - 1]);
-})
+});
 
+// Grab the first word in the input at post it to Firebase
 var submit = function(e) {
   e.preventDefault();
 
